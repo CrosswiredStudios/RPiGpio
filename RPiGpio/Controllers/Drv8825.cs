859e51ba0df9182186225f24ef09ea66770b636f
Matthew Wood <mattwood2855@gmail.com>
2018-03-11 18:31:35 -0700

    stepper

--- RPiGpio/Controllers/Drv8825.cs
+++ RPiGpio/Controllers/Drv8825.cs
@@ -10,24 +10,41 @@ namespace RPiGpio.Controllers
     public class Drv8825
     {
         public enum StepDirection { Clockwise, CounterClockwise };
-
-               
+        public enum StepMode { Full, Half, Quarter, Eigth, Sixteenth, ThirtySecond}
+        
         bool isEnabled;
 
-        public StepDirection Direction { get; }
+        public StepDirection Direction { get; private set; }
         public GpioPin DirectionPin { get; }
         public GpioPin EnablePin { get; }
+        public StepMode Mode { get; private set; }
+        public float StepAngle { get; }
         public GpioPin StepPin { get; }
 
-        public Drv8825(int directionPin, int enablePin, int stepPin, StepDirection direction = StepDirection.Clockwise)
+        public Drv8825(int directionPin, int enablePin, int stepPin)
         {
             var gpioController = GpioController.GetDefault();
 
             DirectionPin = gpioController.OpenPin(directionPin);
+            DirectionPin.Write(GpioPinValue.Low);
+            DirectionPin.SetDriveMode(GpioPinDriveMode.Output);
+            
             EnablePin = gpioController.OpenPin(enablePin);
+            EnablePin.Write(GpioPinValue.Low);
+            EnablePin.SetDriveMode(GpioPinDriveMode.Output);
+            
+            
             StepPin = gpioController.OpenPin(stepPin);
+            StepPin.Write(GpioPinValue.Low);
+            StepPin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        public void Initialize(StepDirection direction = StepDirection.Clockwise, StepMode stepMode = StepMode.Full, bool isEnabled = true)
+        {
             Direction = direction;
-            isEnabled = false;
+            IsEnabled = isEnabled;
+            Mode = stepMode;
+            //StepperMotor = stepperMotor;
         }
 
         public bool IsEnabled
